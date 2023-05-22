ab863dd340305f6f85d0b3d8700dcc2e24191254
emmanuelojiji <emmanuelojiji@hotmail.com>
2023-05-22 16:54:45 +0100

    Initial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <header>
+      <h3>MONDAY 22</h3>
+      <h1>Live Updates</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/Views/Feed.js
@@ -0,0 +1,17 @@
+import "./Feed.scss";
+
+const Feed = () => {
+  return (
+    <>
+    <div className="search-container">
+        <input type="text"></input>
+    </div>
+
+    <div className="feed-container">
+
+    </div>
+    </>
+  )
+};
+
+export default Feed;
