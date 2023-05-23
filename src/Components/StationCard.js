c2690f914632ca0e6a9566c475c43c0504500611
emmanuelojiji <emmanuelojiji@hotmail.com>
2023-05-23 01:15:57 +0100

    Icon added to station card

--- src/Components/StationCard.js
+++ src/Components/StationCard.js
@@ -1,9 +1,13 @@
-import "./StationCard.scss"
+import "./StationCard.scss";
+import icon_train from "../Media/icon-train.svg";
 
-const StationCard = ({name}) => {
-  return <div className="StationCard">
-    <h3>{name}</h3>
-  </div>;
+const StationCard = ({ name }) => {
+  return (
+    <div className="StationCard">
+      <img src={icon_train} />
+      <h3>{name}</h3>
+    </div>
+  );
 };
 
 export default StationCard;
