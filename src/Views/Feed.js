71b6c06b35caf475ab07b36361b73fb68f795870
emmanuelojiji <emmanuelojiji@hotmail.com>
2023-05-24 00:12:34 +0100

    Image preload experiment

--- src/Views/Feed.js
+++ src/Views/Feed.js
@@ -7,6 +7,10 @@ import icon_scroll from "../Media/icon-scroll.svg";
 import icon_train_sideways from "../Media/icon-train-sideways.svg";
 
 const Feed = () => {
+
+  const image = new Image() 
+  image.src = icon_train_sideways;
+  
   const [currentList, setCurrentList] = useState(stations);
   const [userInput, setUserInput] = useState();
 
