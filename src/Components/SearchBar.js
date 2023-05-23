cc7d4d2ec5468b8fc274061f7af9250253e5a901
emmanuelojiji <emmanuelojiji@hotmail.com>
2023-05-23 01:25:39 +0100

    location pin added to search bar

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,15 +1,19 @@
 import "./SearchBar.scss";
+import icon_location_pin from "../Media/icon-location-pin.svg";
 
 const SearchBar = ({ onChange }) => {
   return (
     <div className="search-wrap">
-      <div className="search-location">London</div>
-    <input
-      type="text"
-      placeholder="Search for a station"
-      className="SearchBar"
-      onChange={onChange}
-    ></input>
+      <div className="search-location">
+        <img src={icon_location_pin} />
+        London
+      </div>
+      <input
+        type="text"
+        placeholder="Search for a station"
+        className="SearchBar"
+        onChange={onChange}
+      ></input>
     </div>
   );
 };
