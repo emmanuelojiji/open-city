dfaff492648519007fc93cf5005d0e5e4b5c50c0
emmanuelojiji <emmanuelojiji@hotmail.com>
2023-05-24 01:02:27 +0100

    Card expands on click

--- src/Components/StationCard.js
+++ src/Components/StationCard.js
@@ -1,12 +1,23 @@
 import "./StationCard.scss";
+import { useState } from "react";
 import icon_train from "../Media/icon-train.svg";
 
 const StationCard = ({ name }) => {
+  const [expanded, setExpanded] = useState(false);
   return (
-    <div className="StationCard">
-      <img src={icon_train} />
-      <h3>{name}</h3>
-      <div className="status-light"></div>
+    <div className="StationCard" onClick={() => setExpanded(!expanded)}>
+      <div className="station-card-header">
+        <img src={icon_train} />
+        <h3>{name}</h3>
+        <div className="status-light"></div>
+      </div>
+
+      <div
+        class="card-expanded-info"
+        style={{ height: expanded ? "100px" : "0px" }}
+      >
+        <p>extra info</p>
+      </div>
     </div>
   );
 };
